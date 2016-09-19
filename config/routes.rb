fb9dd730bece124c82680bd08c50a48d3bf57233
thomasjacobjr <thomasjacobjr@gmail.com>
2016-09-19 12:17:12 -0700

    Checkpoint 43 - Voting - Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :labels, only: [:show]
 
   resources :topics do
     resources :posts, except: [:index]
@@ -7,11 +6,16 @@ Rails.application.routes.draw do
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote 
   end
 
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
+
+  resources :labels, only: [:show]
 
   get 'about' => 'welcome#about'
 
