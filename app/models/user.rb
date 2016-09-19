f6778ff7fa33433b86c998cf22282c93ba399c43
thomasjacobjr <thomasjacobjr@gmail.com>
2016-09-18 19:29:20 -0700

    Checkpoint 39 - Posts and Users - Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :posts
   before_save { self.email = email.downcase if email.present? }
 
   validates :name, length: { minimum: 1, maximum: 100}, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
-  validates :password, length: { minimum: 6 }, allow_blank: true 
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
             presence: true,
